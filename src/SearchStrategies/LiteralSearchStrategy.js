c72250c4f19b99a98fd7b2f9902d4d005cddc19d
Neil Boyd <NeilBoyd@gmail.com>
2024-08-08 04:57:41 +0000

    don't use trailing space as exact search indicator

--- src/SearchStrategies/LiteralSearchStrategy.js
+++ src/SearchStrategies/LiteralSearchStrategy.js
@@ -4,30 +4,25 @@ module.exports = new LiteralSearchStrategy()
 
 const segmenter = new Intl.Segmenter([], { granularity: 'word' })
 
-function LiteralSearchStrategy () {
+function LiteralSearchStrategy() {
   this.matches = function (str, crit) {
-    if (!str) return false
+    if (!str) {
+      return false
+    }
     str = str.trim().toLowerCase()
+    crit = crit.trim().toLowerCase()
 
-    let exact = false
-    if (crit.endsWith(' ')) {
-      exact = true
-    }
     if (crit.startsWith('"') && crit.endsWith('"')) {
-      exact = true
-      crit = crit.substring(1, crit.length - 1)
-    }
-    crit = crit.toLowerCase()
-    let critArray = [crit]
-    if (!exact) {
+      let critArray = [crit.substring(1, crit.length - 1)]
+    } else {
       const segmentedText = segmenter.segment(crit)
-      critArray = [...segmentedText].filter(s => s.isWordLike).map(s => s.segment)
+      let critArray = [...segmentedText]
+        .filter((s) => s.isWordLike)
+        .map((s) => s.segment)
     }
 
-    return (
-      critArray
-        .filter((word) => str.indexOf(word) >= 0)
-        .length === critArray.length
-    )
+    let filter = critArray.filter((word) => str.indexOf(word) >= 0)
+
+    return filter.length === critArray.length // true if it found all the words
   }
 }
