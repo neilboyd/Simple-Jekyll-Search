da0e3de944e388f7b86f2899af95491d12b8a3c9
Neil Boyd <neilboyd@gmail.com>
2025-05-11 11:14:11 +0000

    release

--- dest/simple-jekyll-search.js
+++ dest/simple-jekyll-search.js
@@ -172,27 +172,27 @@ function search (crit) {
     return []
   }
   if (opt.sort) {
-    return findMatches(data, crit, opt.searchStrategy, opt).sort(opt.sort).splice(0, opt.limit)
+    return findMatches(data, crit, opt).sort(opt.sort).splice(0, opt.limit)
   }
-  return findMatches(data, crit, opt.searchStrategy, opt)
+  return findMatches(data, crit, opt)
 }
 
 function __setOptions_3 (_opt) {
   opt = _opt || {}
 
-  opt.fuzzy = _opt.fuzzy || false
-  opt.limit = _opt.limit || 10
-  opt.searchStrategy = _opt.fuzzy ? _$FuzzySearchStrategy_4 : _$LiteralSearchStrategy_5
-  opt.exclude = _opt.exclude || []
+  opt.fuzzy = opt.fuzzy || false
+  opt.limit = opt.limit || 10
+  opt.searchStrategy = opt.fuzzy ? _$FuzzySearchStrategy_4 : _$LiteralSearchStrategy_5
+  opt.exclude = opt.exclude || []
 }
 
-function findMatches (data, crit, strategy, opt) {
+function findMatches (data, crit, opt) {
   const matches = []
   for (let i = 0; i < data.length; i++) {
     if (!opt.sort && matches.length >= opt.limit) {
       break
     }
-    const match = findMatchesInObject(data[i], crit, strategy, opt)
+    const match = findMatchesInObject(data[i], crit, opt)
     if (match) {
       matches.push(match)
     }
@@ -200,9 +200,9 @@ function findMatches (data, crit, strategy, opt) {
   return matches
 }
 
-function findMatchesInObject (obj, crit, strategy, opt) {
+function findMatchesInObject (obj, crit, opt) {
   for (const key in obj) {
-    if (key !== 'query' && !isExcluded(obj[key], opt.exclude) && strategy.matches(obj[key], crit)) {
+    if (key !== 'query' && !isExcluded(obj[key], opt.exclude) && opt.searchStrategy.matches(obj[key], crit)) {
       return obj
     }
   }
